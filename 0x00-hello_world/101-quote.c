99dbeae433b9451f01904c5a23b01f450b7831ee
sHAHDwALEED315 <shahdwaleed20017@gmail.com>
2023-09-11 22:53:18 -0700

    commited

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descreption : A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
