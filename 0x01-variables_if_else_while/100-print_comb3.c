de696c4fcf2f38dd9beb1db0f982b0f0a2191ac3
sHAHDwALEED315 <shahdwaleed20017@gmail.com>
2023-09-19 02:57:25 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main 
+ *
+ * Description: Write a program that prints all possible
+ * different combinations of two digits.
+ * 01 and 10 are considered the same combination of the two digits 0 and 1.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
