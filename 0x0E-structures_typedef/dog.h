f0e6af15a42faec3f39a4591e9c27ce75e5ea7c8
sHAHDwALEED315 <shahdwaleed20017@gmail.com>
2023-10-10 13:31:38 -0700

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,14 +3,9 @@
 
 struct dog
 {
-	char*  name;
+	char * name;
 	float  age;
-	char*  owner;
+	char *owner;
 }
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
